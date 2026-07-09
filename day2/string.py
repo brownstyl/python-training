b93bf5547b5c4790f3df421c68fd31586246a9fd
godeh <gabbyjessie44@gmail.com>
2026-07-09 14:47:55 +0100

    day2

---
+++ day2/string.py
@@ -0,0 +1,35 @@
+
+def small_letter(input: str) -> str:
+    conv = bytearray(input.encode('ascii'))
+    for i in range(len(conv)):
+        if 65 <= conv[i] <= 90:
+            conv[i] += 32
+    return conv.decode('ascii')
+
+print(small_letter("HELLO WORLD"))
+
+#===============string maipulation================#
+def cap(input: str) -> str:
+    data = open(input, "r")
+    info = data.read()
+    data.close()
+
+    byte_convo = info.split()
+    for i in range(len(byte_convo)):
+        if byte_convo[i] == "(low)":
+            byte_convo[i-1] = byte_convo[i-1].upper()
+            byte_convo[i] = ""
+            final = []
+            for word in byte_convo:
+                if word != "":
+                 final.append(word)
+
+            join = " ".join(final)
+
+    writeout = open("result.txt", "w")
+    writeout.write(join)
+    writeout.close()
+    return join
+
+
+print(cap("file.txt"))
