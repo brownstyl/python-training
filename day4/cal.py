41789c2ef8ca3e17329ece026d37559bbdca5c56
godeh <gabbyjessie44@gmail.com>
2026-07-14 13:22:43 +0100

    day3

---
+++ day4/cal.py
@@ -0,0 +1,31 @@
+def Add(a, b): return a+b
+
+def Subtract(a, b): return a -b
+
+def Multiply(a, b): return a*b
+
+def Divide(a, b):
+    if b == 0:
+        return 0
+    else:
+        return a/b
+    
+def main():
+    first = input("Provide First number: ")
+    second = input("Provide Second Number: ")
+
+    operator_list = ["+", "-", "/", "*"]
+    print(operator_list, end=" ")
+    
+    key = input(f"\nchose an operator: ")
+    functions = [Add, Subtract, Divide, Multiply]
+
+    
+
+   
+
+       
+
+
+main()
+
