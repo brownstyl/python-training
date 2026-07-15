2c830a2ffc8fdbdd6a42239a1f17378b107e0d4a
godeh <gabbyjessie44@gmail.com>
2026-07-15 01:47:43 +0100

    system.py

---
+++ day4/system.py
@@ -0,0 +1,11 @@
+developer_details = {
+    "name": "Brown",
+    "age": 25,
+    "favourite_language": "English",
+    "expr_yr": "2years",
+    "current_learn": "python",
+}
+ 
+details = developer_details
+
+print(f"My Name Is {details["name"]}, And I Am {details["age"]}years old, which my favorite language is {details["favourite_language"]}, also i have {details["expr_yr"]} of experience, and i'm currently learning {details["current_learn"]}.")
