a38de47321a32722c725d6b61e1a91117f875b7c
godeh <gabbyjessie44@gmail.com>
2026-07-16 13:49:12 +0100

    dictionary pracical hands on drill

---
+++ day5/dictionary.py
@@ -0,0 +1,14 @@
+student_details = {
+    "name": "Gabriel odeh",
+    "age": 20,
+    "class": "primary 2",
+    "grade": 200
+}
+
+list_storage = list(student_details.items())
+print(list_storage)
+
+#for key, value in student_details.items():
+   # print(key, value)
+	#list_storage.append([key, value])
+#print(list_storage)
