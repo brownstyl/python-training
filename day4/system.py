af49aeb8da66cd2a9aa7056796a5350a50f1b99a
godeh <gabbyjessie44@gmail.com>
2026-07-16 10:45:25 +0100

    robust reverse

--- day4/system.py
+++ day4/system.py
@@ -9,3 +9,44 @@ developer_details = {
 details = developer_details
 
 print(f"My Name Is {details["name"]}, And I Am {details["age"]}years old, which my favorite language is {details["favourite_language"]}, also i have {details["expr_yr"]} of experience, and i'm currently learning {details["current_learn"]}.")
+
+#=================student data=============#
+
+student_details = {
+    "name": "",
+    "age": 0, 
+    "class": "",
+}
+
+name = student_details["name"] = "odeh gabriel"
+age = student_details["age"] =20
+student_class = student_details["class"] = "primary 4"
+
+print(student_details)
+
+del(student_details["age"])
+print(student_details)
+
+print()
+def reverse_string():
+    s = "hello"
+    # Your code here
+    st = s.encode('ascii')
+    res = bytearray()
+
+    for i in st:
+        res.insert(0, i)
+      
+    return res.decode('ascii')
+    
+print(reverse_string())
+
+
+s = "hello"
+
+def reverse_robust_string(s: str) -> str:
+    return s[::-1]
+
+w = "hello"
+
+print(reverse_robust_string(w))
