a7548c5bccf5edd9001364796987b4bc0fc6269f
godeh <gabbyjessie44@gmail.com>
2026-07-17 19:14:35 +0100

    call-back registery

--- day6/arithemetic.py
+++ day6/arithemetic.py
@@ -19,36 +19,57 @@ print(evens_set)
 print("============================================")
 
 #test 1
-list_os_cubes = [n**3 for n in range(1, 10)]
+list_os_cubes = [n**3 for n in range(1, 11)]
 print(list_os_cubes)
 
 #test 2
-even_numbers = [n for n in range(1,32) if n % 2 == 0]
+even_numbers = [n for n in range(1,31) if n % 2 == 0]
 print(even_numbers)
 
 #test 3
 list_of_items = ["go", "python", "ai"]
-dic_len = {len(n):[n] for n in list_of_items if len(n) > 0}
+dic_len = {n: len(n) for n in list_of_items if len(n) > 0}
 
 print(dic_len)
 
+#test 4
 
+words = ["cat", "car", "dog", "duck", "cow"]
 
+first_letter = {w[0] for w in words}
 
-example2 = list_of_items
-print(example2)
+print(first_letter)
 
-pri = {
-  2: ['go', 'ai'],
-  6: ['python']
+print("=================================================")
+print("============callback registery================")
+#piece 1#
+
+def shout(text: str):
+  return text.upper()+"!"
+
+def whisper(text: str):
+  return text.lower()+"..."
+
+def reverse(text: str):
+  return text[::-1]
+
+#piece 2#
+commands = {
+  "shout": shout,
+  "whisper": whisper,
+  "reverse": reverse
 }
 
-adder = {}
+#piece 3#
+def run(command: str, text: str)-> str:
+   
+    try:
+      return commands[command](text)
+    except KeyError:
+      print(F"your command '{command}' is invalid")
+      return
+    
 
-for n in pri:
-  if len(n) > 0:
-    adder[len(n)].append(n)
-
-print(adder)
-
-#test 4
+print(run("whisper", "hello"))
+print(run("s", "hello"))
+print(run("reverse", "hello"))
