c36d9bafc8e8f14403e0cc2ae3d29ffadac11dd4
godeh <gabbyjessie44@gmail.com>
2026-07-23 17:13:12 +0100

    day8

--- day9/main.py
+++ day9/main.py
@@ -1,1 +1,41 @@
 ##nested dictionary.....
+
+student = {
+    "name": "Gabriel Odeh",
+    "profile": {
+        "bio": "my name is gabriel and i am a student of talent nation.",
+        "age": 23,
+        "monitization status": False
+    },
+    "skills": {
+        "languages": ["Python", "Go"],
+        "frameworks": ["FastAPI"]
+    }
+}
+
+print(student["profile"]["bio"])
+
+print()
+print("==============Assignment============")
+
+employee = {
+    "name": "Gabriel odeh",
+    "Department": "Computer Ai/Machine learning",
+
+    "contact": {
+        "phone": "08111883595",
+        "email": "gabbyjessie44@mail.com"
+    },
+    "address": {
+        "city": "otukpo",
+        "state": "benue state",
+        "country": "nigeria"
+    }
+}
+print(employee["contact"]["email"])
+print(employee["address"]["city"])
+
+employee["contact"]["phone"] = "09169270082"
+print(employee["contact"]["phone"])
+
+print
